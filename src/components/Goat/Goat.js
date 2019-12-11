1b9f1ac08f81dfdfcc7667c3268440f4413d9a4a
Callan Morrison <callan.l.morrison@gmail.com>
2019-12-10 20:19:20 -0600

    Conditionally show button, useGoat button, available goats

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import goatShape from '../../helpers/propz/goatShape';
 
+import './Goat.scss';
+
 class Goat extends React.Component {
   static propTypes = {
     goat: goatShape.goatShape,
     freeGoat: PropTypes.func,
+    useGoat: PropTypes.func,
   }
 
   freeGoatEvent = (e) => {
@@ -14,19 +17,30 @@ class Goat extends React.Component {
     freeGoat(goat.id);
   }
 
+  useGoatEvent = (e) => {
+    const { useGoat, goat } = this.props;
+    e.preventDefault();
+    useGoat(goat.id);
+  }
+
   render() {
     const { goat } = this.props;
 
     return (
-    <div className="card">
-        <img className="card-img-top" src={goat.imgUrl} alt="Card cap" />
-        <div className="card-body">
-            <h5 className="card-title">{goat.name}</h5>
-            <p className="card-text">{goat.age}</p>
-            <p className="card-text">{goat.description}</p>
-            <div className="btn" onClick={this.freeGoatEvent} >Free Me</div>
+      <div className="Goat col-3">
+        <div className="card">
+          <img className="card-img-top" src={goat.imgUrl} alt="Card cap" />
+          <div className="card-body">
+              <h5 className="card-title">{goat.name}</h5>
+              <p className="card-text">{goat.age}</p>
+              <p className="card-text">{goat.description}</p>
+              {
+                (goat.isBusy) ? (<div className="btn btn-danger" onClick={this.freeGoatEvent} >Free Me</div>)
+                  : (<div className="btn btn-info" onClick={this.useGoatEvent} >Use Me</div>)
+              }
+          </div>
         </div>
-    </div>
+      </div>
     );
   }
 }
--- src/components/GoatCoral/GoatCoral.js
+++ src/components/GoatCoral/GoatCoral.js
@@ -8,19 +8,20 @@ class GoatCoral extends React.Component {
   static propTypes = {
     butts: PropTypes.arrayOf(goatShape.goatShape),
     freeGoat: PropTypes.func,
+    useGoat: PropTypes.func,
   }
 
   render() {
     const myGoats = this.props.butts;
-    const { freeGoat } = this.props;
+    const { freeGoat, useGoat } = this.props;
 
-    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} freeGoat={freeGoat} />);
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} freeGoat={freeGoat} useGoat={useGoat} />);
 
     return (
-        <div className="goatCoral">
+        <div className="GoatCoral d-flex flex-wrap">
             {goatCards}
         </div>
-      );
+    );
   }
 }
 
